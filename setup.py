3b9e6a83d1bdfab71bdcd7ad18b2689f67124515
Sagar Tewari <sagartewariym@yahoo.com>
2018-08-25 13:35:21 +0530

    bumber version to 0.2

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='xontrib-autojump',
-    version='0.1',
+    version='0.2',
     url='https://github.com/gsaga/autojump-xonsh',
     license='MIT',
     author='Sagar Tewari',
