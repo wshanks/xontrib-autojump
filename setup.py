2b604bc0d08933658ed3d086570095827389b249
Sagar Tewari <sagartewariym@yahoo.com>
2018-08-25 14:31:51 +0530

    package is now on pypi

--- setup.py
+++ setup.py
@@ -1,13 +1,17 @@
 from setuptools import setup
 
+long_description = open('README.md').read()
+
 setup(
     name='xontrib-autojump',
-    version='0.2',
+    version='0.6',
     url='https://github.com/gsaga/autojump-xonsh',
     license='MIT',
     author='Sagar Tewari',
     author_email='iaansagar@gmail.com',
     description='autojump support for xonsh',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
     packages=['xontrib'],
     package_dir={'xontrib': 'xontrib'},
     package_data={'xontrib': ['*.xsh']},
