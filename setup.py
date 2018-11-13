d3fde69c2166693105729940c6824487e3a77eb8
Sagar Tewari <sagartewariym@yahoo.com>
2018-11-13 22:43:06 +0530

    updated version

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ long_description = open('README.md').read()
 
 setup(
     name='xontrib-autojump',
-    version='0.7.1',
+    version='0.8',
     url='https://github.com/gsaga/autojump-xonsh',
     license='MIT',
     author='Sagar Tewari',
